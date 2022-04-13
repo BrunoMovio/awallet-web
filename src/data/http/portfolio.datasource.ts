5f61b313ba9923a51c19ff810cef3e63c68cddb0
jeanleeb <jeanleeb@outlook.com.br>
2022-04-13 09:59:03 -0300

    nova url da api

--- src/data/http/portfolio.datasource.ts
+++ src/data/http/portfolio.datasource.ts
@@ -1,6 +1,6 @@
 import { walletData } from "../../modules/home/dashboard/dashboard-data";
 
-const API_URL = 'http://ec2-18-206-87-206.compute-1.amazonaws.com:8888/';
+const API_URL = 'http://ec2-44-193-204-160.compute-1.amazonaws.com:5000/';
 
 export const getPortfolioData = async () => {
   return fetch(API_URL + 'portfolio_analytics', { method: 'POST', body: JSON.stringify(walletData), headers: { 'content-type': 'application/json', 'accept': '*/*' } });
